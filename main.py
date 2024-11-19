2cc2de9593af2e624755a4f0c7cbe4e772d1622c
Snorre Aaldstedt <snorrealdstedt@gmail.com>
2024-11-19 18:10:45 +0100

    Edit main.py Edit main.py to test Autotokenizer etc.

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from llama_cpp import Llama
 
-if __name__ == "__main__":
-    #repo_test_input = input("1 for normistral-7b-warm-instruct, 2 for normistral-7b-warm: ")
+
+def test_llama_cpp_model():
     print("Running main.py")
     #if repo_test_input == "1":
     #    repo_id_name="norallm/normistral-7b-warm-instruct",  # HuggingFace repository containing the GGUF files.
@@ -41,5 +41,29 @@ Hva kan jeg bruke einstape til?<|im_end|>
         ]
     )
 
+def test_gen():
+    tokenizer = AutoTokenizer.from_pretrained("norallm/normistral-7b-warm-instruct")
+    model = AutoModelForCausalLM.from_pretrained("norallm/normistral-7b-warm-instruct", torch_dtype=torch.bfloat16)
 
 
+    messages = [
+        {"role": "user", "content": "Hva er hovedstaden i Norge?"},
+        {"role": "assistant", "content": "Hovedstaden i Norge er Oslo. Denne byen ligger i den sørøstlige delen av landet, ved Oslofjorden. Oslo er en av de raskest voksende byene i Europa, og den er kjent for sin rike historie, kultur og moderne arkitektur. Noen populære turistattraksjoner i Oslo inkluderer Vigelandsparken, som viser mer enn 200 skulpturer laget av den berømte norske skulptøren Gustav Vigeland, og det kongelige slott, som er den offisielle residensen til Norges kongefamilie. Oslo er også hjemsted for mange museer, gallerier og teatre, samt mange restauranter og barer som tilbyr et bredt utvalg av kulinariske og kulturelle opplevelser."},
+        {"role": "user", "content": "Gi meg en liste over de beste stedene å besøke i hovedstaden"}
+    ]
+    gen_input = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt")
+    model.generate(
+        gen_input,
+        max_new_tokens=1024,
+        top_k=64,  # top-k sampling
+        top_p=0.9,  # nucleus sampling
+        temperature=0.3,  # a low temparature to make the outputs less chaotic
+        repetition_penalty=1.0,  # turn the repetition penalty off, having it on can lead to very bad outputs
+        do_sample=True,  # randomly sample the outputs
+        use_cache=True  # speed-up generation
+    )
+
+if __name__ == "__main__":
+    test_gen()
+    #test_llama_cpp_model()
+    #repo_test_input = input("1 for normistral-7b-warm-instruct, 2 for normistral-7b-warm: ")
