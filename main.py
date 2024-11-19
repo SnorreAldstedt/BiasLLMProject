460a2ea0040f6164bbadbd026bd838cf6a89f26a
Snorre Aaldstedt <snorrealdstedt@gmail.com>
2024-11-19 17:40:11 +0100

    Edit main.py
    Now main.py only tests normistral-warm-instruct

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from llama_cpp import Llama
 
 if __name__ == "__main__":
-    repo_test_input = input("1 for normistral-7b-warm-instruct, 2 for normistral-7b-warm: ")
+    #repo_test_input = input("1 for normistral-7b-warm-instruct, 2 for normistral-7b-warm: ")
     print("Running main.py")
-    if repo_test_input == "1":
-        repo_id_name="norallm/normistral-7b-warm-instruct",  # HuggingFace repository containing the GGUF files.
-    else:
-        repo_id_name="norallm/normistral-7b-warm",
+    #if repo_test_input == "1":
+    #    repo_id_name="norallm/normistral-7b-warm-instruct",  # HuggingFace repository containing the GGUF files.
+    #else:
+    #    repo_id_name="norallm/normistral-7b-warm",
     llm = Llama.from_pretrained(
           # HuggingFace repository containing the GGUF files.
-        repo_id=repo_id_name,  # HuggingFace repository containing the GGUF files.
+        repo_id="norallm/normistral-7b-warm-instruct",  # HuggingFace repository containing the GGUF files.
         filename="*Q4_K_M.gguf", # suffix of the filename containing the level of quantization. 
         n_ctx=32768,  # The max sequence length to use - note that longer sequence lengths require much more resources
         n_threads=8,            # The number of CPU threads to use, tailor to your system and the resulting performance
