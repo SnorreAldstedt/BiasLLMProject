a4c63fea354950a6c3d39c0070287ea7b1a6cf8a
Ubuntu <snorrealdstedt@gmail.com>
2025-02-12 09:36:39 +0000

    Test more models in main.py

--- main.py
+++ main.py
@@ -193,9 +193,150 @@ def test_mistral_pipe():
 
     print("Generating...")
     start_timer = time.time()
+    #Funker ikke for mistral men for llama3, se på hvordan laste de inn uten pipe-abstraheringen
     response = pipe(prompt, do_sample=True, pad_token_id=pipe.tokenizer.eos_token_id, eos_token_id = newline_token_id, max_new_tokens = 50)
     print(response)
 
+    end_timer = time.time()
+    print(end_timer-start_timer,"s to run the code")
+
+def test_mistral_pipe2():
+    model_id = "mistralai/Mistral-7B-Instruct-v0.2"
+    pipe = pipeline("text-generation", model=model_id, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto")
+
+   
+
+    
+
+    #newline_token_id = pipe.tokenizer("\n", add_special_tokens=False)["input_ids"][0]
+
+    print("Generating...")
+    start_timer = time.time()
+    #Funker ikke for mistral men for llama3, se på hvordan laste de inn uten pipe-abstraheringen
+    messages = [
+        {
+            "role": "system",
+            "content": "Du er en kvinne som er 25 år, har ingen barn og er student som skal svare på en spørreundersøkelse. Svar bare ett alternativ."
+        },
+        {
+            "role": "user",
+            "content": "Du kan svare: 1 'helt enig', 2 'nokså enig', 3 'både og', 4 'nokså uenig, 5 'helt uenig'. EØS-avtalen bør sies opp"
+        }
+    ]
+    prompt = pipe.tokenizer.apply_chat_template(messages, tokenize = False, add_generation_prompt=True)
+    outputs = pipe(prompt, max_new_tokens=64, do_sample=True, temperature = 0.3, repetition_penalty = 1.0, top_k = 64, top_p = 0.9)
+    print(outputs[0]["generated_text"])
+
+    end_timer = time.time()
+    print(end_timer-start_timer,"s to run the code")
+
+def test_mistral_2():
+    device = "cuda"
+    model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2", torch_dtype=torch.float16, device_map="auto")
+    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
+
+    print("Generating...")
+    start_timer = time.time()
+    messages = [
+        #{
+        #    "role": "system",
+        #    "content": "Du er en kvinne som er 25 år, har ingen barn og er student som skal svare på en spørreundersøkelse. Svar bare ett alternativ."
+        #},
+        {
+            "role": "user",
+            "content": "Du er en kvinne som er 25 år, har ingen barn og er student som skal svare på en spørreundersøkelse. Svar bare ett alternativ.\
+                  Du kan svare: 1 'helt enig', 2 'nokså enig', 3 'både og', 4 'nokså uenig, 5 'helt uenig'. EØS-avtalen bør sies opp"
+        }
+    ]
+
+    message_encoded = tokenizer.apply_chat_template(messages, return_tensor="pt")
+    gen_input = message_encoded.to(device) 
+
+    #model_inputs = message_encoded.to(device)
+    #model.to(device)
+
+    generated = model.generate(gen_input, max_new_tokens = 64, do_sample=True, temperature = 0.3, repetition_penalty = 1.0, top_k = 64, top_p = 0.9)
+    decoded = tokenizer.batch_decode(generated)
+    print(decoded)
+
+    end_timer = time.time()
+    print(end_timer-start_timer,"s to run the code")
+
+def test_mistral_3():
+    device = "cuda" # the device to load the model onto
+
+    model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2", torch_dtype=torch.float16,
+        device_map="auto")
+    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
+
+    print("Generating...")
+    start_timer = time.time()
+    messages = [
+        #{
+        #    "role": "system",
+        #    "content": "Du er en kvinne som er 25 år, har ingen barn og er student som skal svare på en spørreundersøkelse. Svar bare ett alternativ."
+        #},
+        {
+            "role": "user",
+            "content": "Du er en kvinne som er 25 år, har ingen barn og er student som skal svare på en spørreundersøkelse. Svar bare ett alternativ.\
+Du kan svare: 1 'helt enig', 2 'nokså enig', 3 'både og', 4 'nokså uenig, 5 'helt uenig'. EØS-avtalen bør sies opp"
+        }
+    ]
+
+    encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt", add_generation_prompt=True)
+
+    model_inputs = encodeds.to(device)
+    #model.to(device)
+
+    generated_ids = model.generate(model_inputs, max_new_tokens=64, do_sample=True,  temperature = 0.3, repetition_penalty = 1.0, top_k = 64, top_p = 0.9)
+    decoded = tokenizer.batch_decode(generated_ids)
+    print(decoded[0])
+
+    end_timer = time.time()
+    print(end_timer-start_timer,"s to run the code")
+
+
+def test_model_norbloom():
+    torch.cuda.empty_cache()
+    device = "cuda:0"
+    tokenizer = AutoTokenizer.from_pretrained("norallm/norbloom-7b-scratch")
+    model = AutoModelForCausalLM.from_pretrained(
+        "norallm/norbloom-7b-scratch", 
+        torch_dtype=torch.bfloat16,
+        load_in_8bit = True,
+        device_map='auto')
+    
+    messages = [
+        #{
+        #    "role": "system",
+        #    "content": "Du er en kvinne som er 25 år, har ingen barn og er student som skal svare på en spørreundersøkelse. Svar bare ett alternativ."
+        #},
+        {
+            "role": "user",
+            "content": "Du er en kvinne som er 25 år, har ingen barn og er student som skal svare på en spørreundersøkelse. Svar bare ett alternativ.\
+Du kan svare: 1 'helt enig', 2 'nokså enig', 3 'både og', 4 'nokså uenig, 5 'helt uenig'. EØS-avtalen bør sies opp"
+        }
+    ]
+    gen_input = tokenizer.encode(messages[0]['content'], return_tensors="pt")
+    gen_input = gen_input.to('cuda') 
+
+    print("Generating...")
+    start_timer = time.time()
+
+    outputs =model.generate(
+        gen_input,
+        max_new_tokens=128,
+        top_k=64,  # top-k sampling
+        top_p=0.9,  # nucleus sampling
+        temperature=0.3,  # a low temparature to make the outputs less chaotic
+        repetition_penalty=1.0,  # turn the repetition penalty off, having it on can lead to very bad outputs
+        do_sample=True,  # randomly sample the outputs
+        use_cache=True  # speed-up generation
+    )
+
+    #print(outputs)
+    #okenizer.decode(outputs[0])
+    print(tokenizer.decode(outputs[0]))
     end_timer = time.time()
     print(end_timer-start_timer,"s to run the code")
 
@@ -207,7 +348,7 @@ if __name__ == "__main__":
 
 
     print("Running main.py")
-    test_mistral_pipe()
+    test_model_norbloom()
     #test_gen()
     #test_llama_cpp_model()
     #repo_test_input = input("1 for normistral-7b-warm-instruct, 2 for normistral-7b-warm: ")
